1b211fe42c3b1f8a2fc0fb1e17fc445f80dd4377
21las21 <sasha.kudasov04@gmail.com>
2015-05-27 22:33:04 +0300

    Много всего

--- test.py
+++ test.py
@@ -1,24 +1,84 @@
 # спросить имя
 # поздороваться
+# выбрать время решения примеров
+# выбрать режим
 # загадать числа
-# спросить результат сложения
+# спросить результат сложения или вычитания
 # проверить и огласить результат
 
 import random
 
-number1 = random.randrange(1, 11)
+import time
 
-number2 = random.randrange(1, 11)
+name = input("Как тебя зовут? ")
 
-guess = input(str(number1)+"+"+str(number2)+"=")
+print("Привет")
+
+anstf = []
+
+timelimit = input("Скажи, "+name+",сколько секунд тебе нужно для решения нескольких примеров:")
+
+difficult = input("Выбери режим: сложение или вычитание? ")
 
 while True:
 
-    guess = int(guess)
+        if difficult in ["сложение", "вычитание"]:
+            break
+        else:
+            difficult = input("Просто выбери режим: ")
 
-    if guess == number1+number2:
-        print("Молодец! Ты угадал!")
-        break
+startTime = time.time()
+
+questions = 0
+
+answers = 0
+
+num = 1
+
+while True:
+
+    number1 = random.randrange(1, 11)
+
+    number2 = random.randrange(1, 11)
+
+    if difficult == "сложение":
+        guessStr = input(str(num)+"."+str(number1)+"+"+str(number2)+"=")
+    
+    if difficult == "вычитание":
+        guessStr = input(str(num)+"."+str(number1)+"-"+str(number2)+"=")
+
+    questions = questions+1
+
+    num = num + 1
+
+    while True:
+        try:
+            guess = int(guessStr)
+        except:
+            guessStr = input("Нужно ввести число а не ерунду! Введи заново: ")
+        else:
+            break
+            
+    if difficult == "сложение" and number1+number2==guess:
+        answers = answers + 1
+    elif difficult == "вычитание"and number1-number2==guess:
+        answers = answers + 1
+
+    if difficult == "сложение" and number1+number2==guess or difficult == "вычитание"and number1-number2==guess:
+        anstf.append(True)
     else:
-        guess = input("Неудача. Попробуй ещё. ")
+        anstf.append(False)
+
+    timelimit = float(timelimit)    
+
+    if time.time()-startTime > timelimit:
+        print("Твой счёт"+" "+str(answers)+" "+"из"+" "+str(questions))
+        for i in range(0, len(anstf)):
+            if anstf[i] == True:
+                print(str(i + 1) + ". Правильно")
+            else:
+                print(str(i + 1) + ". Неправильно")
+        break
+
+    print ("Осталось"+" "+str(int(timelimit-(time.time()-startTime)))+" "+"секунд")
 
